6cd29fa4515388a7ca9e64588d6befb318129319
Daniel Vrátil <me@dvratil.cz>
2024-06-06 10:10:29 +0200

    Implement handling of PASETO tokens for authentication

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::net::SocketAddr;
+use axum::body::Body;
 use axum::extract::{Query, WebSocketUpgrade, ConnectInfo, State};
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Error, Router};
 use axum::routing::get;
@@ -9,6 +11,7 @@ use serde::Deserialize;
 use tokio::net::TcpListener;
 use tracing_subscriber;
 use slog::Logger;
+use rusty_paseto::prelude::*;
 
 #[macro_use]
 extern crate lazy_static;
@@ -32,6 +35,12 @@ struct Args {
 
     #[arg(short, long, default_value_t = String::from("0.0.0.0") )]
     bind: String
+}
+
+lazy_static! {
+    pub static ref PASETO_SECRET_KEY: Option<PasetoSymmetricKey<V4, Local>> = option_env!("MAILINER_PASETO_SECRET").and_then(|key| {
+        Some(PasetoSymmetricKey::<V4, Local>::from(Key::from(key.as_bytes())))
+    });
 }
 
 #[tokio::main]
@@ -62,9 +71,29 @@ struct Proxy {
 }
 
 
+fn validate_token(token: &String, log: &Logger) ->Result<(), StatusCode> {
+    if cfg!(debug_assertions) && token == "testtoken" {
+        return Ok(());
+    }
+
+    if PASETO_SECRET_KEY.is_none() {
+        crit!(log, "PASETO_SECRET_KEY variable is not set, rejecting client!");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    match PasetoParser::<V4, Local>::default().parse(&token, &PASETO_SECRET_KEY.as_ref().unwrap()) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(StatusCode::UNAUTHORIZED)
+    }
+}
+
 #[debug_handler]
 async fn proxy(query: Query<Proxy>, ws: WebSocketUpgrade, ConnectInfo(addr): ConnectInfo<SocketAddr>, State(log): State<Logger>) -> impl IntoResponse {
     let log = log.new(o!("client" => addr.to_string()));
+
+    if let Err(err) = validate_token(&query.token, &log) {
+        return Err::<Body, StatusCode>(err).into_response();
+    }
 
     info!(log, "Incoming WS connection");
     let fail_log = log.clone();
